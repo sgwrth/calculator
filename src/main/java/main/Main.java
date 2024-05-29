63a6d2efe47cd1cd8af5c5c40bcfbbed330cc672
Andreas Siegwarth <andreassiegwarth@startmail.com>
2024-05-29 10:13:16 +0200

    beginning rewrite of GUI in JavaFX

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,48 @@
 package main;
 
+import java.io.File;
+import java.net.URI;
+
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.Group;
+import javafx.scene.image.*;
+import javafx.scene.paint.Color;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import controller.*;
 import model.Calculator;
 import view.CalculatorView;
-import controller.*;
 
+public class Main extends Application {
+
+	public final String STAGE_TITLE = "Calculator";
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) {
+		try {
+			// BorderPane root = new BorderPane();
+			// Scene scene = new Scene(root,400,400);
+			Group root = new Group();
+			Scene scene = new Scene(root,Color.BLUE);
+			stage.setTitle(STAGE_TITLE);
+			Image icon = new Image(getClass().getResource("/icon.png").toURI().toString());
+			stage.getIcons().add(icon);
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+/*
 import javax.swing.*;
 
 public class Main {
@@ -19,3 +58,4 @@ public class Main {
     }
 
 }
+*/
